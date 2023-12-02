d39300b2eaa2c3289386f5f50eec66ffbb535ba3
hhesse024 <118235862+hhesse024@users.noreply.github.com>
2023-12-02 17:12:51 -0600

    Add files via upload

--- DoomsdayDB_misc.sql
+++ DoomsdayDB_misc.sql
@@ -39,3 +39,175 @@ BEGIN
     SET TaskStatusID = 2
     WHERE TaskStatusID = 1 AND PersonTaskStartDate <= CONVERT(DATE, GETDATE());
 END;
+
+GO
+--Query/View to locate the richest people the factions they are in
+CREATE OR ALTER VIEW Richest_People AS
+SELECT TOP 20
+    PersonFirstName + ' ' + PersonLastName AS [Name],
+    DATEDIFF(YEAR, PersonDateOfBirth, GETDATE()) AS [Age],
+    FactionName AS [Faction],
+    FactionInfluence AS [Faction Influence],
+    CAST(SUM(cp.CurrencyAmount * c.CurrencyValue) AS MONEY) AS [Money on Hand]
+FROM DoomsdayDatabase.dbo.Person p 
+JOIN DoomsdayDatabase.dbo.Faction f ON p.FactionID = f.FactionID
+JOIN DoomsdayDatabase.dbo.CurrencyPerson cp ON p.PersonID = cp.PersonID
+JOIN DoomsdayDatabase.dbo.Currency c ON c.CurrencyID = cp.CurrencyID
+GROUP BY 
+    PersonFirstName + ' ' + PersonLastName,
+    FactionName,
+    FactionInfluence,
+    PersonDateOfBirth
+ORDER BY [Money on Hand] DESC;
+
+
+-- Trigger to Delete factions when their influence reaches 0
+CREATE TRIGGER trg_UpdateFaction
+ON [dbo].[Faction]
+INSTEAD OF UPDATE
+AS
+	DECLARE @factionID INT;
+	DECLARE @newinfluence INT;
+	DECLARE @oldinfluence INT;
+	DECLARE @factionName NVARCHAR(35);
+
+	SELECT @factionID = i.FactionID FROM inserted i;
+	SELECT @newinfluence = i.FactionInfluence FROM inserted i;
+	SELECT @oldinfluence = d.FactionInfluence FROM deleted d;
+	SELECT @factionName = d.FactionName FROM deleted d;
+
+	BEGIN
+		IF EXISTS(SELECT * FROM inserted i JOIN deleted d ON i.FactionID = d.FactionID WHERE i.FactionInfluence <= 0)
+			BEGIN
+				DELETE FROM DoomsdayDatabase.dbo.FactionLocation WHERE FactionID IN (SELECT FactionID FROM deleted);
+				UPDATE DoomsdayDatabase.dbo.Person SET FactionID = NULL WHERE FactionID IN (SELECT FactionID FROM deleted);
+				DELETE FROM DoomsdayDatabase.dbo.Faction WHERE FactionID in (SELECT FactionID FROM deleted);
+				PRINT 'Faction ' + @factionName + ' has been disbanded';
+			END
+		ELSE
+			BEGIN
+				UPDATE DoomsdayDatabase.dbo.Faction SET FactionInfluence = (SELECT FactionInfluence from inserted) WHERE FactionID in (SELECT FactionID FROM inserted);
+			END
+	END;
+GO
+
+-- SPROC to get all information on a person into one report
+CREATE PROCEDURE [dbo].[usp_GetPersonInformation]
+    @PersonID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Get basic person information
+    SELECT
+        PersonFirstName + ' ' + PersonLastName AS [Name],
+        PersonDateOfBirth AS [DOB],
+		DATEDIFF(YEAR,PersonDateOfBirth,GETDATE()) AS [Age],
+        PersonCallSign AS [Call Sign],
+        PersonHealth AS [Health %],
+        CASE 
+			WHEN PersonDeceased = 1 THEN 'YES'
+			ELSE 'No'
+		END AS [Is Deceased],
+        PersonDateOfDeath AS [Date of Death],
+        l.LocationName AS [Location],
+		lod.LodgingName AS [Lodging Type],
+        f.FactionName AS [Faction]
+
+    FROM
+        dbo.Person p
+    LEFT JOIN
+        dbo.LocationLodging ll ON p.LocationLodgingID = ll.LocationLodgingID
+    LEFT JOIN
+        dbo.Location l ON ll.LocationID = l.LocationID
+	LEFT JOIN
+		Lodging lod ON ll.LodgingID = lod.LodgingID
+    LEFT JOIN
+        dbo.Faction f ON p.FactionID = f.FactionID
+    WHERE
+        p.PersonID = @PersonID;
+
+    -- Get skills information
+    SELECT
+        s.SkillName,
+        ps.PersonSkillProficiency
+    FROM
+        dbo.PersonSkill ps
+    JOIN
+        dbo.Skill s ON ps.SkillID = s.SkillID
+    WHERE
+        ps.PersonID = @PersonID;
+
+    -- Get tasks information
+    SELECT
+        t.TaskName,
+        ts.TaskStatusDescription,
+        pt.PersonTaskStartDate,
+        pt.PersonTaskDueDate
+    FROM
+        dbo.PersonTask pt
+    JOIN
+        dbo.Task t ON pt.TaskID = t.TaskID
+    JOIN
+        dbo.TaskStatus ts ON pt.TaskStatusID = ts.TaskStatusID
+    WHERE
+        pt.PersonID = @PersonID;
+
+	-- Get wallet information
+	SELECT 
+		c.CurrencyName,
+		c.CurrencyValue,
+		cp.CurrencyAmount,
+		CAST(SUM(cp.CurrencyAmount * c.CurrencyValue) AS MONEY) AS [Total Value]
+	FROM 
+		CurrencyPerson cp 
+	JOIN 
+		Currency c ON cp.CurrencyID = c.CurrencyID
+	WHERE
+		cp.PersonID = @PersonID
+	GROUP BY 
+		c.CurrencyName,
+		c.CurrencyValue,
+		cp.CurrencyAmount;
+
+-- Trigger to update influence when a new location and faction are added to FactionLocation table.
+
+CREATE TRIGGER [dbo].[trg_UpdateInfluence]
+ON [dbo].[FactionLocation]
+AFTER INSERT
+AS
+	DECLARE @newfactionID INT;
+	DECLARE @newinfluence INT;
+	
+	SELECT @newfactionID = i.FactionID FROM inserted i;
+	
+
+	BEGIN
+		IF EXISTS(SELECT i.FactionID FROM inserted i JOIN Faction ON i.FactionID = Faction.FactionID)
+			BEGIN
+				UPDATE DoomsdayDatabase.dbo.Faction SET Faction.FactionInfluence = FactionInfluence +1
+				WHERE Faction.FactionID = @newfactionID AND FactionInfluence <=9
+			END
+	END;
+
+
+-- SPROC to update date of death and deceased status when health reaches 0
+CREATE PROCEDURE [dbo].[sp_ZeroHealth]
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN
+    UPDATE Person
+    SET PersonDeceased = 1,
+	PersonDateOfDeath =  CONVERT(Date, GETDATE())
+    WHERE PersonHealth = 0 AND PersonDeceased = 0;
+END
+END;
+
+-- View to find safest locations when danger is imminent
+CREATE VIEW view_SafestLocations
+AS
+SELECT TOP (100) PERCENT LocationName, LocationDescription, LocationSafetyLevel
+FROM     dbo.Location
+GROUP BY LocationSafetyLevel, LocationName, LocationDescription
+ORDER BY LocationSafetyLevel DESC;
